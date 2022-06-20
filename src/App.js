9dc4ba110327c23b4c2dd632228638825c2281e2
minous27 <dill2726@hotmail.com>
2022-06-19 23:21:25 -0500

    Capturing selections to a state and logging it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 import background from './assets/PHEMORE.jpg'
 
 
 function App() {
+  const [useLogicEngine, isUseLogicEngine] = useState(false);
+  const [fileSlot, setFileSlot] = useState(1);
+  const [difficulty, setDifficulty] = useState("basic")
+  const [log, setLog] = useState("");
+
+
+  let onEnterSettingsData = () =>{
+    
+    setLog(
+      "Settings:\n"
+      + "Use Logic Engine = " + useLogicEngine + "\n"
+      + "Fileslot = " + fileSlot + "\n"
+      + "Difficulty = " + difficulty + "\n");
+  }
+
+  let generateRandoFile = () =>{
+    //Will do the work to create the file and download it on client machine.
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,38 +30,40 @@ function App() {
         <h1>SEED ME PHEMORE</h1>
         <img src={background}/>
       </header>
-      <div className='settings'>
-        <div className='logic-checkbox-section'>
-          <label>
-          <input type="checkbox"/>
-          Use Logic Engine
-          </label>
-        </div>
-        <div className='file-slot-section'>
-          <h3 className='section-title'>File Slot</h3>
-          <div className='file-slot-selection'>
-            <input type="radio" className='file-slot-setting' name='file-slot' value='1' checked/>
-            <label>1</label>
-            <br/><input type="radio" className='file-slot-setting' name='file-slot' value='1'/>
-            <label>2</label>
-            <br/><input type="radio" className='file-slot-setting' name='file-slot' value='1'/>
-            <label>3</label>
+      <form>
+        <div className='settings'>
+          <div className='logic-checkbox-section'>
+            <label>
+            <input type="checkbox" onClick={(selection) => {isUseLogicEngine(selection.target.checked)}}/>
+            Use Logic Engine
+            </label>
+          </div>
+          <div className='file-slot-section'>
+            <h3 className='section-title'>File Slot</h3>
+            <div className='file-slot-selection'>
+              <input type="radio" className='file-slot-setting' name='file-slot' value='1' onClick={(selection) => {setFileSlot(selection.target.value)}} defaultChecked/>
+              <label>1</label>
+              <br/><input type="radio" className='file-slot-setting' name='file-slot' value='2' onClick={(selection) => {setFileSlot(selection.target.value)}}/>
+              <label>2</label>
+              <br/><input type="radio" className='file-slot-setting' name='file-slot' value='3' onClick={(selection) => {setFileSlot(selection.target.value)}}/>
+              <label>3</label>
+            </div>
+          </div>
+          <div className='difficulty-section'>
+            <h3 className='section-title'>Difficulty</h3>
+            <input type="radio" className='difficulty-setting' name='difficulty' value="basic" onClick={(selection) => {setDifficulty(selection.target.value)}} defaultChecked/>
+            <label>Basic Settings</label><br/>
+            <input type="radio" className='difficulty-setting' name='difficulty' value="advanced" onClick={(selection) => {setDifficulty(selection.target.value)}}/>
+            <label>Advanced Settings</label>
           </div>
         </div>
-        <div className='difficulty-section'>
-          <h3 className='section-title'>Difficulty</h3>
-          <input type="radio" className='difficulty-setting' name='difficulty' value="basic" checked/>
-          <label>Basic Settings</label><br/>
-          <input type="radio" className='difficulty-setting' name='difficulty' value="advanced"/>
-          <label>Advanced Settings</label>
+        <div>
+          <button type='button' onClick={onEnterSettingsData}>Generate</button>
         </div>
-      </div>
-      <div>
-        <button type='button'>Generate</button>
-      </div>
-      <div>
-        <textarea>Sup</textarea>
-      </div>
+        <div>
+          <textarea disabled value={log}/>
+        </div>
+      </form>
     </div>
   );
 }
